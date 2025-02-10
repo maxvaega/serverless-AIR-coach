74a4bcd503ee7e84e1d3bef77979c041095cb484
anto <98389170+spidyskip@users.noreply.github.com>
2025-02-10 08:55:24 +0000

    Add validation 1 character minimum for userid

--- src/models.py
+++ src/models.py
@@ -1,20 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 # Define response model
 
 class MessageRequest(BaseModel):
     message: str
-    userid: Optional[str] = None
+    userid: str = Field(..., min_length = 1)
 
 class MessageResponse(BaseModel):
     query: str
     result: str
-    userid: str
+    userid: str = Field(..., min_length = 1)
 
-class DatabaseInput(BaseModel):
-    human: str
-    system: str
-    userid: str
-    env : str
-    timestamp : str
