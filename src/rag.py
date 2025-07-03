55223af62c5cae12bb0dff40e909adcea40a9ea4
maxvaega <maxvaega@users.noreply.github.com>
2025-07-03 09:49:20 +0200

    2.5 flash lite

--- src/rag.py
+++ src/rag.py
@@ -75,7 +75,7 @@ combined_docs = get_combined_docs()
 system_prompt = build_system_prompt(combined_docs)
 
 # Define LLM Model
-model = "gemini-2.5-flash"
+model = "gemini-2.5-flash-lite-preview-06-17"
 llm = ChatGoogleGenerativeAI(
     model=model,
     temperature=0.7,
